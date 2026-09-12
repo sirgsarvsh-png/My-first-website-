75bd04269034c1e65c9cce6fa0c722d31ee2a4ce
sirgsarvsh-png <sirgsarvsh@gmail.com>
2026-09-12 15:39:18 +0530

    Add SkillCard and SkillBox components

--- skill.jsx
+++ skill.jsx
@@ -1,1 +1,20 @@
 
+function SkillCard({ name }) {
+  return (
+    <li className="card">{name}</li>
+  );
+}
+
+function SkillBox() {
+  const skills = ["HTML", "CSS", "Teaching"];
+  return (
+    <ul className="skill-box">
+      {skills.map((s) => (
+        <SkillCard key={s} name={s} />
+      ))}
+    </ul>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("react-skill-root"));
+root.render(<SkillBox />);
