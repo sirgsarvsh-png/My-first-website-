b1b829098fed7436ab6e00523201f0a24da3aae1
sirgsarvsh-png <sirgsarvsh@gmail.com>
2026-09-19 07:22:28 +0530

    Fix Like button state initialization and update logic

--- skills.jsx
+++ skills.jsx
@@ -19,9 +19,9 @@ function SkillBox() {
 const root = ReactDOM.createRoot(document.getElementById("react-skill-root"));
 root.render(<SkillBox />);
 function Likebutton() {
-  const [Like, Setlike] = React.useState (___);
+  const [Like, Setlike] = React.useState (0);
   return (
-    <button onClick = {() => setLikes(____)}>
+    <button onClick = {() => setLikes(likes +1)}>
        likes : {likes}
        </button>
     );
